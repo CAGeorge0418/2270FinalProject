d13b300363e1ff77905878fbeb7bd4b27d47553c
D-rew-k <82475909+D-rew-k@users.noreply.github.com>
2021-04-13 15:16:05 -0600

    Update miniGit.hpp

--- miniGit.hpp
+++ miniGit.hpp
@@ -1,10 +1,29 @@
+#ifndef MINIGIT_HPP
+#define MINIGIT_HPP
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstring>
+#include <sstream>
 
 
+struct doublyNode{
+  int commitNumber;
+  singlyNode* head;
+  doublyNode* previous;
+  doublyNode* next;
+};
 
-
-
+struct singlyNode{
+  string fileName;
+  string fileVersion;
+  singlyNode* next;
+};
 
 
 class github{
 
 };
+
+#endif
