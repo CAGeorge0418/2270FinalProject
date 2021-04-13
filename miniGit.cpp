2466b3c5e46cc6afa02dff456f388d5ecebfdc24
D-rew-k <82475909+D-rew-k@users.noreply.github.com>
2021-04-13 15:47:08 -0600

    Update miniGit.cpp

--- miniGit.cpp
+++ miniGit.cpp
@@ -4,3 +4,56 @@
 
 using namespace std;
 
+
+miniGit::miniGit()
+{
+    chief->commitNumber = 0;
+    chief->head = NULL;
+    chief->previous = NULL;
+    chief->next = NULL;
+}
+
+miniGit::~miniGit()
+{
+    doublyNode* tmp_d = chief;
+
+    while(tmp_d != NULL)
+    {
+        singlyNode* temp_s = chief->head;
+
+        while(temp_s != NULL)
+        {
+            chief->head = chief->head->next;
+            delete temp_s;
+            temp_s = chief->head;
+
+        }
+
+        chief = chief->next;
+        delete tmp_d;
+        tmp_d = chief;
+    }
+
+
+}
+
+void miniGit::addFile(string name)
+{
+
+}
+
+void miniGit::removeFile(string name)
+{
+
+}
+
+void miniGit::commit()
+{
+
+}
+
+void miniGit::checkout(int commitNumber)
+{
+
+}
+
