8ea32a4e5f5d48cbb520151bd3685eacdc41f50c
D-rew-k <82475909+D-rew-k@users.noreply.github.com>
2021-04-13 16:24:19 -0600

    Update miniGit.cpp

--- miniGit.cpp
+++ miniGit.cpp
@@ -37,8 +37,53 @@ miniGit::~miniGit()
 
 }
 
-void miniGit::addFile(string name)
+string traverseGit(string name, doublyNode* chief)
 {
+    doublyNode* tmp_d = chief;
+
+    while(tmp_d != NULL)
+    {
+        singlyNode* temp_s = tmp_d->head;
+
+        while(temp_s != NULL)
+        {
+            temp_s = temp_s->next;
+            if(temp_s->fileName == name) return temp_s->fileVersion;
+        }
+        tmp_d = tmp_d->next;
+    }
+
+    return "00";
+}
+
+string miniGit::addFile(string name)
+{
+    ifstream newbie;
+    newbie.open(name);
+    if(!newbie.is_open()) return "File does not exist in directory";
+    newbie.close();
+
+    doublyNode* tmp_d;
+
+    while(tmp_d->next != NULL) tmp_d = tmp_d->next;
+
+    singlyNode* tmp_s = tmp_d->head;
+
+    while(tmp_s->next != NULL) 
+    {
+        if(tmp_s->fileName == name) return "File has already been added";
+        tmp_s = tmp_s->next;
+    }
+
+    singlyNode* adding = new singlyNode;
+    adding->fileName = name;
+    string version = traverseGit(name, chief);
+    adding->fileVersion = name.substr(0,name.size()-3) + "_" + version + name.substr(name.size()-3, 3);
+    adding->next = NULL;
+
+    tmp_s->next = adding;
+
+    return "File successfully added";
 
 }
 
