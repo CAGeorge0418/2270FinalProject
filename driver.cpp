f04d4dc996291a7a8f49b069b868d90e066e7064
CAGeorge0418 <82475871+CAGeorge0418@users.noreply.github.com>
2021-04-14 14:36:38 -0600

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,1 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <io.h>
+#include "miniGit.hpp"
+
+using namespace std;
+
+int main()
+{
+    mkdir("minigit");
+    string filename;
+    cout << "input filename:" << endl;
+    getline(cin, filename);
+    ifstream ifile;
+    ifile.open(filename);
+
+    if (!ifile.is_open())
+    {
+        cout << "could not open " << filename << endl;
+    }
+    else
+    {
+        ofstream ofile;
+        ofile.open("minigit/driver01.cpp");
+        string line = "";
+        while (getline(ifile, line))
+        {
+            ofile << line << endl;
+        }
+        cout << "done" << endl;
+        
+        ofile.close();
+        ifile.close();
+    }
+
+    
+    
+}
