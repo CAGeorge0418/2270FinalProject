122f1ab640e8e91ac5854b60516622eccf45e734
CAGeorge0418 <82475871+CAGeorge0418@users.noreply.github.com>
2021-04-20 21:17:12 -0600

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -25,6 +25,12 @@ int main()
         cout << "6. Quit" << endl;
 
         getline(cin, option);
+        if (option != "1" && option != "2" && option != "3" && option != "4" && option != "5" && option != "6")
+        {
+            option = "0";
+        }
+        
+
         numopt = stoi(option);
 
         switch(numopt)
