62838535e04e647f4f391468854fb415707a52c2
CAGeorge0418 <82475871+CAGeorge0418@users.noreply.github.com>
2021-04-20 20:38:59 -0600

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -21,7 +21,8 @@ int main()
         cout << "2. Remove a File" << endl;
         cout << "3. Commit Changes" << endl;
         cout << "4. Checkout Previous Version" << endl;
-        cout << "5. Quit" << endl;
+        cout << "5. Log of Previous Commits" << endl;
+        cout << "6. Quit" << endl;
 
         getline(cin, option);
         numopt = stoi(option);
@@ -86,7 +87,7 @@ int main()
                     getline(cin, comnum);
                     for (int i = 0; i < comnum.length(); i++)
                     {
-                        if (comnum[i] != '1' || comnum[i] != '2' || comnum[i] != '3' || comnum[i] != '4' || comnum[i] != '5' || comnum[i] != '6' || comnum[i] != '7' || comnum[i] != '8' || comnum[i] != '9' || comnum[i] != '0')
+                        if (comnum[i] != '1' && comnum[i] != '2' && comnum[i] != '3' && comnum[i] != '4' && comnum[i] != '5' && comnum[i] != '6' && comnum[i] != '7' && comnum[i] != '8' && comnum[i] != '9' && comnum[i] != '0')
                         {
                             cout << "Please enter a positive integer" << endl;
                             input = false;
@@ -101,12 +102,20 @@ int main()
             }
             case 5:
             {
+                cout << "Here is the current log of MiniGit:" << endl;
+                cout << "-----------------------------------" << endl;
+                mini.log();
+
+                break;
+            }
+            case 6:
+            {
                 cout << "Goodbye!" << endl;
                 break;
             }
             default:
             {
-                cout << "Please enter a number between 1 and 5." << endl;
+                cout << "Please enter a number between 1 and 6" << endl;
                 break;
             }
         }
